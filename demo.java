894cb4dfae245f173959e4e6703a63d3ff9d260c
Kaviyarasu Raja <kaviyarasuraja55@gmail.com>
2024-12-01 17:03:41 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class demo{
+    public static void main(String[] args)
+    {
+        System.out.println("ENTER NUM");
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for(int i=N;i<=1000;i=i+N){
+        System.out.println(i);
+
+
+}    }}
+
